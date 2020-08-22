b7898f2b6a09a0278420c49cd664943995c1a3fa
julietvt <freshitpt@gmail.com>
2020-08-22 10:08:28 +0300

    add checkbox

--- src/components/UserList/ListItem.js
+++ src/components/UserList/ListItem.js
@@ -3,13 +3,17 @@ import React from 'react';
 const ListItem = (props) => {
   const {
     isSelected,
+    onSelect,
     user: { id, fName, lName },
   } = props;
 
-  const styles = { bgColor: 'rgb(0,0,255)' };
+  const styles = { bgColor: 'rgba(0,0,0,0.5)' };
   return (
     <li style={styles}>
-      UserID: "{id}"; UserFullName:"{fName} {lName}";
+      <span>
+        UserID: "{id}"; UserFullName:"{fName} {lName}";
+      </span>
+      <button onClick={onSelect}>Select</button>
     </li>
   );
 };
--- src/components/UserList/index.js
+++ src/components/UserList/index.js
@@ -1,41 +1,61 @@
 import React, { Component } from 'react';
 import ListItem from './ListItem';
+// parent -> child  props
+// child -> parent callback
+
+const dbData = [
+  {
+    id: 1,
+    fName: 'firstname1',
+    lName: 'lastname1',
+  },
+  {
+    id: 2,
+    fName: 'firstname2',
+    lName: 'lastname2',
+  },
+  {
+    id: 3,
+    fName: 'firstname3',
+    lName: 'lastname3',
+  },
+  {
+    id: 4,
+    fName: 'firstname4',
+    lName: 'lastname4',
+  },
+  {
+    id: 5,
+    fName: 'firstname5',
+    lName: 'lastname5',
+  },
+];
 
 class UserList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      users: [
-        {
-          id: 1,
-          fName: 'firstname1',
-          lName: 'lastname1',
-        },
-        {
-          id: 2,
-          fName: 'firstname2',
-          lName: 'lastname2',
-        },
-        {
-          id: 3,
-          fName: 'firstname3',
-          lName: 'lastname3',
-        },
-        {
-          id: 4,
-          fName: 'firstname4',
-          lName: 'lastname4',
-        },
-        {
-          id: 5,
-          fName: 'firstname5',
-          lName: 'lastname5',
-        },
-      ],
+      users: dbData.map((u) => ({ ...u, isSelected: false })),
     };
   }
-  userMap = (user) => {
-    return <ListItem key={user.id} user={user} />;
+
+  mapUser = (user, index) => {
+    const { users } = this.state;
+    const selectUserH = () => {
+      const newUsers = [...users];
+      newUsers[index].isSelected = !newUsers[index].isSelected;
+      this.setState({
+        users: newUsers,
+      });
+    };
+    return (
+      <ListItem
+        key={user.id}
+        user={user}
+        onSelect={selectUserH}
+        isSelected={user.isSelected}
+      />
+    );
   };
 
   render() {
