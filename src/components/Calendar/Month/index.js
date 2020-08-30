6b8f83113d4b74f9eb47241c5a84f4eafab0b1ef
julietvt <freshitpt@gmail.com>
2020-08-30 12:01:06 +0300

    Calendar: Month was updated

--- src/components/Calendar/Month/index.js
+++ src/components/Calendar/Month/index.js
@@ -1,30 +1,31 @@
 import React from 'react';
 import Week from './Week';
-import styles from './Month.module.css';
-import { getWeek, getWeeksInMonth } from 'date-fns';
-function Month(props) {
-  const {
-    month: { name, number },
-    year,
-    currentDate,
-  } = props;
-  const getWeeks = () => {
-    const firstWeek = getWeek(new Date(year, number, 1));
-    const weeksInMonth = getWeeksInMonth(new Date(year, number, 1));
-    const weeks = [];
-    for (let i = firstWeek; i < firstWeek + weeksInMonth; i++) {
-      weeks.push(
-        <Week key={i} week={i} year={year} currentDate={currentDate} />
-      );
-    }
-    return weeks;
-  };
+import styles from './Month.module.scss';
+import { format, eachWeekOfInterval, startOfMonth, endOfMonth } from 'date-fns';
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
+
+const getWeeks = ({ date, currentDate }) => {
+  return eachWeekOfInterval({
+    start: startOfMonth(date),
+    end: endOfMonth(date),
+  }).map((weekDate) => (
+    <Week
+      key={weekDate}
+      weekDate={weekDate}
+      date={date}
+      currentDate={currentDate}
+    />
+  ));
+};
+
+const Month = (props) => {
+  const { date } = props;
+  const className = classNames(styles.box, styles.dayName);
   return (
-    <div>
-      <div>
-        {name} {year}
-      </div>
-      <ul>
+    <div className={styles.wrapper}>
+      <div className={styles.title}>{format(date, 'LLLL y')}</div>
+      <ul className={styles.row}>
         <li className={styles.dayName}>s</li>
         <li className={styles.dayName}>m</li>
         <li className={styles.dayName}>t</li>
@@ -33,8 +34,16 @@ function Month(props) {
         <li className={styles.dayName}>f</li>
         <li className={styles.dayName}>s</li>
       </ul>
-      <div>{getWeeks()}</div>
+      {getWeeks(props)}
     </div>
   );
-}
+};
+
+Month.propTypes = {
+  date: PropTypes.instanceOf(Date).isRequired,
+  currentDate: PropTypes.instanceOf(Date),
+};
+Month.defaultProps = {
+  currentDate: new Date(),
+};
 export default Month;
