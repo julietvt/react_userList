acca9a4ce253753912e24b0fb003735b9b862e8d
julietvt <freshitpt@gmail.com>
2020-09-05 13:56:47 +0300

    UserLoader: component was added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import Carousel from './components/Carousel';
 import { slidesCollection } from './components/Carousel/config';
+import UserLoader from './components/UserLoader';
 
 const App = () => {
-  return <Carousel width={800} height={400} slides={slidesCollection} />;
+  return <UserLoader />;
 };
 export default App;
