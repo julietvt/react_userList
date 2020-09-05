bb28ae3bac6abc17debdb00705bf4ea2fe62ff6b
julietvt <freshitpt@gmail.com>
2020-09-05 11:50:27 +0300

    Slider carousel: add component Slide

---
+++ src/components/Carousel/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Slide from './Slide';
+import Control from './Control';
+import styles from './Carousel.module.scss';
+
+class Carousel extends Component {
+  constructor(props) {
+    super(props);
+  }
+}
+
+export default Carousel;
