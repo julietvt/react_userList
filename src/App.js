a1f38612cfc2eabaad5ad16340eda8d53def66da
julietvt <freshitpt@gmail.com>
2020-09-06 12:24:47 +0300

    Timer component with hooks

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Carousel from './components/Carousel';
 import { slidesCollection } from './components/Carousel/config';
 import UserLoader from './components/UserLoader';
 import Chat from './components/Chat';
+import Timer from './components/FuncTimer';
 
 const App = () => {
   //return <UserLoader />;
   //return <Carousel width={800} height={400} slides={slidesCollection} />;
   //return <Counter step={2} />;
-  return <Chat />;
+  //return <Chat />;
+  return <Timer />;
 };
 export default App;
