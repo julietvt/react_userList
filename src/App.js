24f9045da08c24cf577fe33e8502bd3a17894614
julietvt <freshitpt@gmail.com>
2020-09-06 11:19:56 +0300

    Counter component (hooks)

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import React, { Component } from 'react';
 import Carousel from './components/Carousel';
 import { slidesCollection } from './components/Carousel/config';
 import UserLoader from './components/UserLoader';
+import Counter from './components/Counter';
 
 const App = () => {
-  return <UserLoader />;
+  //return <UserLoader />;
+  //return <Carousel width={800} height={400} slides={slidesCollection} />;
+  return <Counter step={2} />;
 };
 export default App;
